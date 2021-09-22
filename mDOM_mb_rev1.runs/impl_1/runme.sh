507cf065e861a63764411eaf294d2756e3bae775
mkauer <mkauer@icecube.wisc.edu>
2021-09-22 12:04:28 -0500

    just recompiled 0x20 code to reproduce identical binary

--- mDOM_mb_rev1.runs/impl_1/runme.sh
+++ mDOM_mb_rev1.runs/impl_1/runme.sh
@@ -6,14 +6,10 @@
 # Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 # 
 
-echo "This script was generated under a different operating system."
-echo "Please update the PATH and LD_LIBRARY_PATH variables below, before executing this script"
-exit
-
 if [ -z "$PATH" ]; then
-  PATH=C:/Xilinx/SDK/2019.1/bin;C:/Xilinx/Vivado/2019.1/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2019.1/ids_lite/ISE/lib/nt64:C:/Xilinx/Vivado/2019.1/bin
+  PATH=/home/mkauer/DM_ICE_250/firmware/Xilinx/SDK/2019.1/bin:/home/mkauer/DM_ICE_250/firmware/Xilinx/Vivado/2019.1/ids_lite/ISE/bin/lin64:/home/mkauer/DM_ICE_250/firmware/Xilinx/Vivado/2019.1/bin
 else
-  PATH=C:/Xilinx/SDK/2019.1/bin;C:/Xilinx/Vivado/2019.1/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2019.1/ids_lite/ISE/lib/nt64:C:/Xilinx/Vivado/2019.1/bin:$PATH
+  PATH=/home/mkauer/DM_ICE_250/firmware/Xilinx/SDK/2019.1/bin:/home/mkauer/DM_ICE_250/firmware/Xilinx/Vivado/2019.1/ids_lite/ISE/bin/lin64:/home/mkauer/DM_ICE_250/firmware/Xilinx/Vivado/2019.1/bin:$PATH
 fi
 export PATH
 
@@ -24,7 +20,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='C:/Users/atfie/IceCube/mDOMDevelopment/mdommb_rev1_fw/mDOM_mb_rev1.runs/impl_1'
+HD_PWD='/home/mkauer/DM_ICE_250/firmware/mdommb_rev1_fw/mDOM_mb_rev1.runs/impl_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
@@ -41,7 +37,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log top.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source top.tcl -notrace
 
 
--- mDOM_mb_rev1.srcs/sources_1/ip/mig_7series_0/mig_7series_0_stub.vhdl
+++ mDOM_mb_rev1.srcs/sources_1/ip/mig_7series_0/mig_7series_0_stub.vhdl
@@ -1,10 +1,10 @@
 -- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
--- Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
--- Date        : Sat Jan 16 16:09:00 2021
--- Host        : LAPTOP-GBOUD091 running 64-bit major release  (build 9200)
--- Command     : write_vhdl -force -mode synth_stub
---               C:/Users/atfie/IceCube/mDOMDevelopment/mdommb_rev1_fw/mDOM_mb_rev1.srcs/sources_1/ip/mig_7series_0/mig_7series_0_stub.vhdl
+-- Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
+-- Date        : Wed Jul 14 14:59:50 2021
+-- Host        : matts-laptop running 64-bit Fedora release 32 (Thirty Two)
+-- Command     : write_vhdl -force -mode synth_stub -rename_top mig_7series_0 -prefix
+--               mig_7series_0_ mig_7series_0_stub.vhdl
 -- Design      : mig_7series_0
 -- Purpose     : Stub declaration of top-level module interface
 -- Device      : xc7s100fgga676-2
